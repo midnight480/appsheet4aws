982465475f9860c1717976fee31883e818a6d326
midnight480 <manage-github@ooi-sbr.page>
2023-01-07 22:48:20 +0900

    #2 EventBridgePipeも作成（DDB→SFn）

--- src/lib/resource/iam.ts
+++ src/lib/resource/iam.ts
@@ -6,6 +6,9 @@ export class Iam {
     constructor() { };
     public iamUser: aws_iam.User;
     public iamPolicy: aws_iam.Policy;
+    public iamPolicyDocument: aws_iam.PolicyDocument;
+    public iamRole: aws_iam.Role;
+
 
     public createIamUser(scope: Construct, iamName: string) {
         this.iamUser = new aws_iam.User(scope, iamName, {
@@ -67,5 +70,50 @@ export class Iam {
     public getPolicyName(iamPolicy: aws_iam.Policy) {
         return iamPolicy.policyName;
     }
+
+    public createIamPolicy4ebPipesSource(scope: Construct, iamPolicyName: string) {
+        this.iamPolicy = new aws_iam.Policy(scope, 'Create IAM Policy 4 Source', {
+          policyName: iamPolicyName,
+              statements: [
+                new aws_iam.PolicyStatement({
+                    effect: aws_iam.Effect.ALLOW,
+                    actions: [
+                      'dynamodb:DescribeStream',
+                      'dynamodb:GetRecords',
+                      'dynamodb:GetShardIterator',
+                      'dynamodb:ListStreams',
+                    ],
+                  }),
+            ],
+        });
+        return this.iamPolicy;
+    }                              
+
+    public createIamPolicy4ebPipesTarget(scope: Construct, iamPolicyName: string) {
+      this.iamPolicy = new aws_iam.Policy(scope, 'Create IAM Policy 4 Target', {
+        policyName: iamPolicyName,
+          statements: [
+            new aws_iam.PolicyStatement({
+              effect: aws_iam.Effect.ALLOW,
+              actions: [
+                'state:*',
+              ],
+              resources: [
+                '*',   
+              ],
+            }), 
+          ]
+      },)
+      return this.iamPolicy;
+    }
+    
+    public createIamRole4ebPipes(scope: Construct, iamRoleName: string, inlinePolicySource: aws_iam.Policy, inlinePolicyTarget: aws_iam.Policy ) {
+        const iamRole = new aws_iam.Role(scope, 'Create IAM Role 4 EB Pipes', {
+            assumedBy: new aws_iam.ServicePrincipal('pipes.amazonaws.com'),
+            roleName: iamRoleName,
+        });
+        return iamRole;
+    }
+    
 }
 
--- src/lib/src-stack.ts
+++ src/lib/src-stack.ts
@@ -7,6 +7,9 @@ import { Iam } from './resource/iam';
 import { Sfn } from './resource/sfn';
 import { Eb } from './resource/eventbridge';
 
+import { PolicyDocument,PolicyStatement,Effect, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { CfnPipe } from 'aws-cdk-lib/aws-pipes';
+
 export class SrcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -27,6 +30,11 @@ export class SrcStack extends cdk.Stack {
     const sfnName = 'appsheet4aws-sfn';
     // EventBridge
     const ebName = 'appsheet4aws-eb';
+
+    // EventBridge Pipes
+    const ebPipesSourceIamRoleName = 'appsheet4aws-eb-pipes-source-iam-role';
+    const ebPipesTargetIamRoleName = 'appsheet4aws-eb-pipes-target-iam-role';
+    const ebPipesIamRoleName = 'appsheet4aws-eb-iam-role';
 
     /*--- S3 Task --*/
     const s3bucket = new S3();
@@ -60,13 +68,75 @@ export class SrcStack extends cdk.Stack {
     eb.createEb(this, ebName);
     eb.eventRuleToStepFunction(this, sfn.sfn);
 
+    // eventbridge pipes
+    //const pipesSourcePolicy = iam.createIamPolicy4ebPipesSource(this, ebPipesSourceIamRoleName);
+    //const pipesTargetPolicy = iam.createIamPolicy4ebPipesTarget(this, ebPipesTargetIamRoleName);
+    //const pipesIamRole = iam.createIamRole4ebPipes(this, ebPipesIamRoleName, pipesSourcePolicy, pipesTargetPolicy); 
+    //const pipesIamRole = iam.createIamRole4ebPipes(this, ebPipesIamRoleName); 
+    //pipesIamRole.attachInlinePolicy(pipesSourcePolicy);
+    //pipesIamRole.attachInlinePolicy(pipesTargetPolicy);
+    //const pipes = eb.createEbPipes(this, pipesIamRole, ddb.table, eb.eventbridge);
+    
+    // ********************
+    // eventbridge pipes
+    const sourcePolicy = new PolicyDocument({
+      statements: [
+        new PolicyStatement({
+          resources: [(ddb.table.tableStreamArn as string)],
+          actions: [
+            'dynamodb:DescribeStream',
+            'dynamodb:GetRecords',
+            'dynamodb:GetShardIterator',
+            'dynamodb:ListStreams',
+          ],
+          effect: Effect.ALLOW,
+        }),
+      ],
+    });
+
+    const targetPolicy = new PolicyDocument({
+      statements: [
+        new PolicyStatement({
+          resources: [eb.eventbridge.eventBusArn],
+          actions: ['events:PutEvents','state:*'],
+          effect: Effect.ALLOW,
+        }),
+      ],
+    });
+
+    const pipeRole = new Role(this, 'role', {
+      assumedBy: new ServicePrincipal('pipes.amazonaws.com'),
+      inlinePolicies: {
+        sourcePolicy,
+        targetPolicy,
+      },
+    });
+
+    
+    // Create new Pipe
+    const pipe = new CfnPipe(this, 'pipe', {
+      roleArn: pipeRole.roleArn,
+      source: (ddb.table.tableStreamArn as string), 
+      sourceParameters: {
+        dynamoDbStreamParameters: {
+          startingPosition: cdk.aws_lambda.StartingPosition.LATEST,
+        },
+      },
+      target: sfn.sfn.stateMachineArn,
+    });
+    //************* */
+
+    // Output 
     new cdk.CfnOutput(this, 'S3 Bucket Arn', { value: s3bucket.bucket.bucketArn}) ;
     new cdk.CfnOutput(this, 'DynamoDB Arn', { value: ddb.table.tableArn }) ;
+    //new cdk.CfnOutput(this, 'DynamoDB Arn', { value: (ddb.table.tableStreamArn as string) }) ;
     //new cdk.CfnOutput(this, 'Kinesis Stream Arn', { value: kinesisStream.stream.streamArn }) ;
     new cdk.CfnOutput(this, 'IAM User Name', { value: iamUser.userName }) ;
     new cdk.CfnOutput(this, 'IAM Policy Name', { value: iamPolicy.policyName }) ;
     new cdk.CfnOutput(this, 'Step Functions Arn', { value: sfn.sfn.stateMachineArn }) ;
     new cdk.CfnOutput(this, 'EventBridge Arn', { value: eb.eventbridge.eventBusArn }) ;
+    new cdk.CfnOutput(this, 'EventBridge Pipe Attr Arn', { value: pipe.attrArn }) ;
+    new cdk.CfnOutput(this, 'EventBridge Pipe Role Arn', { value: pipe.roleArn }) ;
 
   }
 }
